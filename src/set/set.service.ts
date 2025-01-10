376df9671bebeb1af69f5f020ba37e024e2d7007
ralmonacid <rodrigo.almonacid@i-car.cl>
2025-01-09 23:13:05 -0300

    feature/add-creation-set-and-update-readme

--- src/set/set.service.ts
+++ src/set/set.service.ts
@@ -11,6 +11,20 @@ export class SetService {
     @InjectRepository(SetEntity)
     private setEntity:Repository<SetEntity>
   ){}
+  async create(createSetDto: CreateSetDto) {
+    const createSet = new SetEntity()
+    createSet.id = createSetDto.id;
+    createSet.logo_url = createSetDto.logo_url;
+    createSet.name = createSetDto.name;
+    createSet.printed_total = createSetDto.printed_total;
+    createSet.ptcgo_code = createSetDto.ptcgo_code;
+    createSet.release_date = createSetDto.release_date;
+    createSet.series = createSetDto.series;
+    createSet.symbol_url = createSetDto.symbol_url;
+    createSet.total = createSetDto.total;
+    createSet.updated_at = createSetDto.updated_at
+    return await this.setEntity.save(createSet);
+  }
 
   //Listar todos los set
   async findAll() {
